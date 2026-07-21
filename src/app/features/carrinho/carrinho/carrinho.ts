dc91a67dc2b2a12fc36c3c1f2a8b739f02dcb377
Luana Cardoso <luanamartinscardoso01@gmail.com>
2026-07-21 13:51:58 -0300

    feat(features/carrinho): cria funcionalidade carrinho

---
+++ src/app/features/carrinho/carrinho/carrinho.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrinho',
+  imports: [],
+  templateUrl: './carrinho.html',
+  styleUrl: './carrinho.css',
+})
+export class Carrinho {
+  //logica do carrinho virá em breve ou em aulas futuras
+  //por hora apenas vai exibir a página de carrinho 
+}
