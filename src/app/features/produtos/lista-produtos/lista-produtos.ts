1873952f74277458b99152cd3efdbdba1329ee46
Luana Cardoso <luanamartinscardoso02@gmail.com>
2026-07-23 13:37:47 -0300

    feat(lista-produtos.ts): cria método de requisição dos produtos, inicia o loading, faz a adapitação da API para o projeto...

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -4,6 +4,7 @@ import {computed} from '@angular/core';
 import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
 import { UpperCasePipe } from '@angular/common';
 import {effect} from '@angular/core'
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-lista-produtos',
@@ -12,28 +13,39 @@ import {effect} from '@angular/core'
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos {
- produtos = signal([
-    { 
-      nome: 'Teclado Gamer', 
-      preco:149.00
-    },
-    {
-      nome: 'Mouse Gamer', 
-      preco:299.99
-    },
-    {
-      nome: 'Monitor Gamer', 
-      preco:1599.99
-    },
-    {
-      nome: 'Desktop Gamer', 
-      preco:4999.99
-    },
-    {
-      nome: 'Headset Gamer', 
-      preco:699.99
+  //! remover a lista de produtos, dados carregados via API fakestoreapi
+ produtos = signal <{
+  nome: string ; preco: number }[]> ([]);
+//? criar estado de carregamento
+//** true: requisição em andamento, exibir indicador no template
+//! false: esconder indicador e exibir a
+  carregando = signal(true);
+
+  //! cria o método para a requisição dos produtos
+  carregarProdutos(){
+    //! iniciar loadging 
+    this.carregando.set(true);
+    this.http.get<{title: string; price: number }[]>
+    ('https://fakestoreapi.com/products')
+    .subscribe({
+      next: (dados) => {
+
+        //!adapta a api para nosso projeto 
+        const produtosFormatados = dados.map(p =>({
+nome: p.title,
+preco:p.price
+        }));
+        this.produtos.set(produtosFormatados);
+        this.carregando.set(false);
+      },
+      //?Finaliza loagding
+      error: (erro) =>{
+        console.error('Erro ao carregar produtos: ', erro);
+        this.carregando.set(false);
+      }
+      });
     }
-  ]);
+
   exibirProduto (nome: string){
     //console.log ('Produto Selecionado: ', nome); 
     this.produtoSelecionado.set(nome);
@@ -56,7 +68,14 @@ export class ListaProdutos {
        {nome: 'Headset', preco: 25},
     ]);
 }
-constructor(){
+//! injetar httpClient dentro de construct, restruturar construct!
+constructor( private http: HttpClient ){
+
+  //! carregar a API
+  
+  this.carregarProdutos();
+ 
+  //! effects continuam iguais 
   effect(() =>{
     console.log('Lista de Produtos Alterados: ', this.produtos())
   });
